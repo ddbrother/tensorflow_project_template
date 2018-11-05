2ae2f083b931ae4ef809c310c5c554014bdf59c0
jiangjixiang <jiangjixiang@aliyun.com>
2018-11-05 20:31:24 +0800

    rename test *.ini scripts

---
+++ mains/main_test_ini.py
@@ -0,0 +1,35 @@
+
+import __init__
+import tensorflow as tf
+
+from data_loader.data_generator import DataGenerator
+from models.example_model import ExampleModel
+from trainers.example_trainer import ExampleTrainer
+from utils.config import process_config
+from utils.dirs import create_dirs
+from utils.logger import Logger
+from utils.utils import get_args
+
+import configparser
+
+
+def main():
+    cp = configparser.ConfigParser()
+    cp.read("test.ini")
+
+    secs = cp.sections()
+    print(cp.sections())
+
+    for sec in secs:
+        opts = cp.options(sec)
+        for opt in opts:
+            val = cp.get(sec, opt)
+            val += "test....."
+            cp.set(sec, opt, val)
+            print(sec, '.', opt, '=', val)
+
+    cp.write(open("out.ini", "w"))
+
+
+if __name__ == '__main__':
+    main()
