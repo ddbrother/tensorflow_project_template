18ef3cfa1be17f6774986a75dfe28770bda00cc2
jiangjixiang <jiangjixiang@aliyun.com>
2018-11-06 14:55:52 +0800

    Add mnist_trainer.py; Modified main_mnist's import modules

--- mains/main_mnist.py
+++ mains/main_mnist.py
@@ -2,9 +2,10 @@
 import __init__
 import tensorflow as tf
 
-from data_loader.data_generator import DataGenerator
-from models.example_model import ExampleModel
-from trainers.example_trainer import ExampleTrainer
+from data_loader.mnist_data_generator import DataGenerator
+from models.mnist_model import ExampleModel
+from trainers.mnist_trainer import ExampleTrainer
+
 from utils.config import process_config
 from utils.dirs import create_dirs
 from utils.logger import Logger
