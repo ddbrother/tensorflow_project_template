2e1788da52050fb13fb211c114afb3b7f5d3854a
jiangjixiang <jiangjixiang@aliyun.com>
2018-11-09 12:17:34 +0800

    test numpy + pyplot OK

--- mains/test_numpy_matplotlib.py
+++ mains/test_numpy_matplotlib.py
@@ -5,7 +5,7 @@ import numpy as np
 
 data = np.ones([5,5], dtype=np.float32) #生成随机数据,5行5列,最大值1,最小值-1
 data[0,0] = 0
-data[0,1] = 0
-im = plt.imshow(data, cmap=plt.cm.gray)
-
+plt.imshow(data, cmap=plt.cm.gray)
+plt.xlabel("test x label")
+plt.title("test title")
 plt.show()
